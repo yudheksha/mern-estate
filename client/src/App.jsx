b669d9692450d3fefa972c4536bcdb857dbfdba0
yudheksha <yudithaagk@gmail.com>
2024-06-19 10:36:00 -0400

    update the header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from './pages/Profile'
 import About from './pages/About'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 export default function App() {
   return (
@@ -18,7 +19,9 @@ export default function App() {
       <Route path="/sign-in" element={<SignIn/>} />
       <Route path="/sign-up" element={<SignUp/>} />
       <Route path="/about" element={<About/>} />
-      <Route path="/profile" element={<Profile/>} />
+      <Route element = {<PrivateRoute/>} >
+         <Route path="/profile" element={<Profile/>} />
+      </Route>
 
 
     </Routes>
