813c320ed4a2d6e98961a32b736b26faa0832e15
yudheksha <yudithaagk@gmail.com>
2024-06-19 09:00:00 -0400

    create and add OAuth component

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
+import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
   const { loading , error } = useSelector((state) => state.user);
-  // const [error, setError] = useState(null);
-  // const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -22,7 +21,6 @@ export default function SignIn() {
     e.preventDefault();
 
     try {
-      // setLoading(true);
       dispatch(signInStart());
 
       const res = await fetch('/api/auth/signin', {
@@ -35,23 +33,17 @@ export default function SignIn() {
       const data = await res.json();
       if (data.success === false) {
         dispatch(signInFailure(data.message));
-        // setLoading(false);
-        // setError(data.message);
         return;
       }
-      // setLoading(false);
-      // setError(null);
       dispatch(signInSuccess(data));
       navigate('/');
-      // console.log(data);
     } catch (error) {
-      // setLoading(false);
-      // setError(error.message);
       dispatch(signInFailure(error.message));
     }
   };
 
-  console.log(formData);
+  // console.log(formData);
+  // console.log(er)
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -80,6 +72,7 @@ export default function SignIn() {
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
+        <OAuth/>
       </form>
 
       <div className='flex gap-2 mt-5'>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -36,7 +36,6 @@ export default function SignUp() {
       setLoading(false);
       setError(null);
       navigate('/sign-in');
-      // console.log(data);
     } catch (error) {
       setLoading(false);
       setError(error.message);
@@ -80,6 +79,7 @@ export default function SignUp() {
         >
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
+        <OAuth/>
       </form>
 
       <div className='flex gap-2 mt-5'>
