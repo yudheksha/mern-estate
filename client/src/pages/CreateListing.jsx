016a40324d6a951ac377970a3b1dead9ea6e6925
yudheksha <yudithaagk@gmail.com>
2024-07-20 15:32:49 -0400

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -299,7 +299,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular price</p>
-                <span className="text-xs">($ / month)</span>
+                { formData.type === 'rent' && (
+                  <span className="text-xs">($ / month)</span>
+                )}
               </div>
             </div>
 
@@ -318,7 +320,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Discounted Price</p>
-                <span className="text-xs">($ / month)</span>
+                { formData.type === 'rent' && (
+                  <span className="text-xs">($ / month)</span>
+                )}s
               </div>
             </div>
             )}
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -322,7 +322,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular price</p>
-                <span className="text-xs">($ / month)</span>
+                { formData.type === 'rent' && (
+                  <span className="text-xs">($ / month)</span>
+                )}
               </div>
             </div>
 
@@ -340,7 +342,9 @@ export default function CreateListing() {
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted Price</p>
-                  <span className="text-xs">($ / month)</span>
+                  { formData.type === 'rent' && (
+                    <span className="text-xs">($ / month)</span>
+                  )}
                 </div>
               </div>
             )}
