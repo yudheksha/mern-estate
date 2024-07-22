c3509b2bd0ce4cd47f4b8ba22b4e4ace4dd07916
yudheksha <yudithaagk@gmail.com>
2024-07-22 12:32:37 -0400

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -2,7 +2,6 @@ import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 import { isValidObjectId } from "../utils/objectIdValidator.js";
 
-
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
@@ -25,7 +24,7 @@ export const deleteListing = async (req, res, next) => {
 
   try {
     await Listing.findByIdAndDelete(req.params.id);
-    res.status(200).json('Listing has been deleted!');
+    res.status(200).json("Listing has been deleted!");
   } catch (error) {
     next(error);
   }
@@ -44,18 +43,16 @@ export const updateListing = async (req, res, next) => {
     const listing = await Listing.findById(id);
     if (!listing) {
       console.error(`Listing not found for id: ${id}`);
-      return next(errorHandler(404, 'Listing not found!'));
+      return next(errorHandler(404, "Listing not found!"));
     }
 
     if (req.user.id !== listing.userRef.toString()) {
       return next(errorHandler(401, "You can only update your own listings!"));
     }
 
-    const updatedListing = await Listing.findByIdAndUpdate(
-      id,
-      req.body,
-      { new: true }
-    );
+    const updatedListing = await Listing.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
 
     console.log(`Listing updated successfully for id: ${id}`);
     res.status(200).json(updatedListing);
@@ -65,19 +62,64 @@ export const updateListing = async (req, res, next) => {
   }
 };
 
-export const getListing = async(req,res,next) =>{
-
-  try{
-
+export const getListing = async (req, res, next) => {
+  try {
     const listing = await Listing.findById(req.params.id);
-    if(!listing) {
-      return next(errorHandler(404, 'Listing not found!'));
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"));
     }
-    
-    res.status(200).json(listing);
 
-  } catch(error) {
+    res.status(200).json(listing);
+  } catch (error) {
     next(error);
   }
+};
 
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+    .sort({ [sort]: order })
+    .limit(limit)
+    .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
 };
